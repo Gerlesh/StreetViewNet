83ae31c0d4ecd602df9a5007ce91e3d5e368ceb7
Petar Przulj <petar.przulj@mail.utoronto.ca>
2023-03-23 17:02:37 -0400

    Updated k-means.py to save 22means model, Added labeler.py to convert img to feature space

--- k-means.py
+++ k-means.py
@@ -1,16 +1,16 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 if __name__ == "__main__":
 	loaded = np.load('blue_points_indicies.npz')
 	x = loaded["x"]
 	y = loaded["y"]
 	X = np.stack((x, y), axis=1)
-
-	kmeans_models = []
 	
 	'''
+	kmeans_models = []
 	scores = []
 	num_clusters = [5,10,20,50,100,150,200]
 	for i in range(len(num_clusters)):
@@ -19,8 +19,8 @@ if __name__ == "__main__":
 		scores.append(kmeans_models[i].score(X))
 	# plt.figure(1)
 	plt.plot(num_clusters, scores)
-	plt.show() ''' # around 20 is the best number of clusters
-
+	plt.show() # around 20 is the best number of clusters '''
+	'''
 	for i in range(20,26):
 		kmeans = KMeans(n_clusters=i, random_state=0).fit(X)
 		plt.figure(i)
@@ -28,7 +28,12 @@ if __name__ == "__main__":
 		plt.plot(x,y, "o", markersize=0.5)
 		plt.plot(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], "o", markersize=5)
 		plt.show()
-		kmeans_models.append(kmeans)
-	np.save("kmeans_models.npy", kmeans_models)
+		kmeans_models.append(kmeans) '''
+	kmeans = KMeans(n_clusters=22, random_state=0).fit(X)
+	plt.plot(x,y, "o", markersize=0.5)
+	plt.plot(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], "o", markersize = 5)
+	plt.show()
+	
+	pickle.dump(kmeans, open("kmeans22.sav", 'wb'))
 
 	
