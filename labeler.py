26df13f8aca08c4be858cbc91bcd4fb3ca2b0128
Petar Przulj <petar.przulj@mail.utoronto.ca>
2023-04-15 13:53:16 -0400

    labler.py now resizes and labels images

--- labeler.py
+++ labeler.py
@@ -3,7 +3,6 @@ import numpy as np
 from argparse import ArgumentParser
 from PIL import Image
 from pyproj import Transformer
-import re
 import pickle
 
 TRANSFORMER = Transformer.from_crs("EPSG:4326","EPSG:3857")
@@ -19,29 +18,25 @@ if __name__ == "__main__":
 	parser = ArgumentParser()
 	parser.add_argument("-d","--directory",default="dataset")
 	parser.add_argument("-m","--model", default="kmeans22.sav")
-	parser.add_argument("-o","--output", default="dataset.npz")
+	parser.add_argument("-o","--outputdir", default="resized")
+	parser.add_argument("-s","--size", default=256, type=int)
 	args = parser.parse_args()
 
-	if not os.path.isdir(args.directory) or not os.path.isfile(args.model):
+	if not os.path.isdir(args.directory) or not os.path.isfile(args.model) or not os.path.isdir(args.outputdir):
 		exit("Directory does not exist")
 
-	lat_longs = np.loadtxt(os.path.join(args.directory,"lat_long_images.txt"))
-
 	kmeans = pickle.load(open(args.model, 'rb'))
-
-	imgs = []
-	labels = []
-	headings = []
+	labels = np.zeros(kmeans.n_clusters)
 	for file in os.listdir(args.directory):
-		if file.startswith("gsv_"):
-			lat_long = lat_longs[int(re.search(r"\d+", file).group(0))]
-			x,y = get_x_y(lat_long[0], lat_long[1])
-			label = kmeans.predict(np.array([x,y]).reshape(1,-1))
-			im = np.array(Image.open(os.path.join(args.directory, file)))
-			imgs.append(im)
-			labels.append(label[0])
-			headings.append(lat_long[2])
-	np.savez_compressed(os.path.join(args.directory,args.output), imgs=imgs, labels=labels, headings=headings)
+		if file.endswith(".jpg"):
+			image = Image.open(os.path.join(args.directory, file))
+			new_image = image.resize((args.size, args.size))
+			lat_long = file[:-4].split()
+			x,y = get_x_y(float(lat_long[0]), float(lat_long[1]))
+			label = kmeans.predict(np.array([x,y]).reshape(1,-1))[0]
+			new_image.save(os.path.join(args.outputdir, file[:-4]+f" {label}.jpg"))
+			labels[label] += 1
+	print(labels)
 
 
 			
